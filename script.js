78810662d5c21a73b4437c68934dfe47f672fc15
Freja <frej400d@stud.kea.dk>
2022-03-19 15:28:56 +0100

    card container animation

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-let headerText;
-let tl;
+gsap.registerPlugin(ScrollTrigger);
 
 window.addEventListener("DOMContentLoaded", start);
 
 function start() {
   console.log("Welcome to my Portfolio");
-  headerText = document.querySelector(".header-text-ani");
-  tl = gsap.timeline({ repeat: 0, repeatDelay: 1 });
+  let headerText = document.querySelector(".header-text-ani");
+  let tl = gsap.timeline({ repeat: 0, repeatDelay: 1 });
 
   tl.to(headerText, { duration: 0.75, opacity: 0, scale: 0.6 });
   tl.to(headerText, { duration: 2.5, opacity: 1, scale: 1 });
+
+  cardContainerAni();
+}
+function cardContainerAni() {
+  let cardContainer = document.querySelector(".card-container");
+  gsap.to(cardContainer, {
+    duration: 3,
+    y: "-10vw",
+    opacity: 1,
+    scrollTrigger: {
+      trigger: ".assignments-container",
+      scrub: 0.5,
+      markers: true,
+      start: "top 50%",
+      end: "bottom 65%",
+    },
+  });
 }
